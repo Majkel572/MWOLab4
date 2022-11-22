41038fc66f160f52429329a35074cf4746fa5b54
Mikołaj <mrefe12345@gmail.com>
2022-11-22 19:45:24 +0100

    little changes

---
+++ MWOLab4-AzurePipeline/MWOLab4-AzurePipeline/BlinkBlinkBoy.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MWOLab4_AzurePipeline {
+    public class BlinkBlinkBoy : PageModel{
+        private readonly ILogger<BlinkBlinkBoy> logger;
+        public string Version { get; set; }
+
+        public BlinkBlinkBoy(ILogger<BlinkBlinkBoy> _logger) {
+            this.logger = _logger;
+            string versionFile = "versions.txt";
+            string? versionText = null;
+            if (System.IO.File.Exists(versionFile)) {
+                versionText = System.IO.File.ReadAllText(versionFile);
+            }
+            if(versionText is null or "") {
+                Version = "Lack of versions";
+            } else {
+                Version = versionText;
+            }
+        }
+    }
+}
